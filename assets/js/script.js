b5f38244a37cde8d574e844b156a3033f6749b6f
samanthashleyrose <samantha.rose327@gmail.com>
2023-10-30 21:30:22 -0400

    Movie title now displays when submit btn clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,61 +5,75 @@ const movieRefreshBtn = document.getElementById('refresh-movie-btn');
 var edamamAppID = "d55761af"
 var edamamAPIKey = "4dd514756747177099b4472f62002d79"
 var watchModeAPIKey = "xsVPrQhegCVFwP7yQKzZsWsGMKT7VMQE6ZiwJmgi"
-var movieGenre = "Action"
-        
+
 function getEdamamRecipe() {
     var searchQuery = document.getElementById('recipe-input').value
-    var edamamRecipeURL = "https://api.edamam.com/api/recipes/v2?type=public&q="+searchQuery+"&app_id="+edamamAppID+"&app_key=" + edamamAPIKey +"&random=true"
+    var edamamRecipeURL = "https://api.edamam.com/api/recipes/v2?type=public&q=" + searchQuery + "&app_id=" + edamamAppID + "&app_key=" + edamamAPIKey + "&random=true"
 
     fetch(edamamRecipeURL)
         .then(function (response) {
             return response.json()
-    })
+        })
         .then(function (data) {
             console.log("RECIPE", data)
             var randomRecipeNumber = Math.floor(Math.random() * data.hits.length)
             console.log("RANDOM RECIPE NUMBER", randomRecipeNumber);
 
-        // RECIPE TITLE
-        let recipeTitle = data.hits[randomRecipeNumber].recipe.label;
-        console.log("RECIPE TITLE", recipeTitle);
+            // RECIPE TITLE
+            let recipeTitle = data.hits[randomRecipeNumber].recipe.label;
+            console.log("RECIPE TITLE", recipeTitle);
 
-        // RECIPE IMAGE
-        let recipeImage = data.hits[randomRecipeNumber].recipe.image;
-        console.log("RECIPE IMAGE", recipeImage);
+            // RECIPE IMAGE
+            let recipeImage = data.hits[randomRecipeNumber].recipe.image;
+            console.log("RECIPE IMAGE", recipeImage);
 
-        let populateRecipeImage = document.createElement('img');
-        populateRecipeImage.setAttribute('src',recipeImage)
+            let populateRecipeImage = document.createElement('img');
+            populateRecipeImage.setAttribute('src', recipeImage)
 
-        // RECIPE LINK 
-        let recipeLink = data.hits[randomRecipeNumber].recipe.shareAs;
-        console.log("RECIPE LINK", recipeLink);
-        
-        // POPULATES RECIPE CONTAINER
-        let recipeContainer = document.querySelector('.recipe-container')
-        let recipeCard = document.createElement('a');
+            // RECIPE LINK 
+            let recipeLink = data.hits[randomRecipeNumber].recipe.shareAs;
+            console.log("RECIPE LINK", recipeLink);
 
-        recipeCard.setAttribute('class','recipe-card')
-        recipeCard.textContent = recipeTitle;
-        recipeCard.setAttribute('href', recipeLink);
-        recipeCard.appendChild(populateRecipeImage);
-        recipeContainer.appendChild(recipeCard);
-    })
+            // POPULATES RECIPE CONTAINER
+            let recipeContainer = document.querySelector('.recipe-container')
+            let recipeCard = document.createElement('a');
+
+            recipeCard.setAttribute('class', 'recipe-card')
+            recipeCard.textContent = recipeTitle;
+            recipeCard.setAttribute('href', recipeLink);
+            recipeCard.appendChild(populateRecipeImage);
+            recipeContainer.appendChild(recipeCard);
+        })
 }
 
-var movieURL = 'https://api.watchmode.com/v1/list-titles/?append_to_response=sources&apiKey=' + watchModeAPIKey + "&genres="+movieGenre
-
 function getMovie() {
+    // var movieGenre = document.getElementById('genre-input').value
+    var movieGenre = document.getElementById('genre-input').value
+    var movieURL = 'https://api.watchmode.com/v1/list-titles/?append_to_response=sources&apiKey=' + watchModeAPIKey + "&genres=" + movieGenre
+
     fetch(movieURL)
-    .then(function (apple){
-        return apple.json()
-    })
-    .then(function (banana){
-        console.log(banana)
-        var randomMovieNumber = Math.floor(Math.random() * banana.titles.length)
-        console.log(randomMovieNumber)
-        console.log(banana.titles[randomMovieNumber])
-    })
+        .then(function (apple) {
+            return apple.json()
+        })
+        .then(function (banana) {
+            console.log('BANANA', banana)
+            var randomMovieNumber = Math.floor(Math.random() * banana.titles.length)
+            console.log(randomMovieNumber)
+            console.log(banana.titles[randomMovieNumber])
+
+            // MOVIE TITLE
+            let movieTitle = banana.titles[randomMovieNumber].title;
+            console.log("MOVIE TITLE", movieTitle);
+
+            // POPULATES MOVIE CONTAINER
+            let movieContainer = document.querySelector('.movie-container')
+            let movieCard = document.createElement('a');
+
+            movieCard.setAttribute('class', 'movie-card');
+            movieCard.textContent = movieTitle;
+            movieContainer.appendChild(movieCard);
+        })
+
 }
 
 // Event Listener for Recipe Submit Button
@@ -73,6 +87,7 @@ function handleRecipeSubmitBtn(event) {
 function handleMovieSubmitBtn(event) {
     event.preventDefault()
     console.log('Movie Submit Button Clicked');
+    getMovie();
 };
 
 // Event Listener for Recipe Refresh Button
